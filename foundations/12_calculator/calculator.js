e515052efdc3a2604b1a98f99dbfb3ef8c9944af
Ulrik K. Wichmann <28593903+ulle82@users.noreply.github.com>
2026-04-06 20:17:30 +0200

    Completed exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const result = arr.reduce((a, b) => a + b, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const resultMult = arr.reduce((a, b) => a * b, 1);
+  return resultMult;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  return Array.from({ length: num }, (_, i) => i + 1).reduce((a, b) => a * b, 1)
 };
 
 // Do not edit below this line
